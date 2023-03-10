b3e9869cb85189e3875080de836ca71245377838
srinivasmaram2023 <127029950+srinivasmaram2023@users.noreply.github.com>
2023-03-10 17:55:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-5fm6wa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5fm6wa)
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <h1>Hello World</h1>
+        <p>This is react-web-app</p>
+        <h2>Reactjs is a JavaScriptLibrary to build effective interfaces</h2>
+        <p>Some features of react are </p>
+        <ul>
+          <li>Effective User interfaces</li>
+          <li>Virtual DOM methods</li>
+          <li>Ahead of time</li>
+          <li>Unified User experienxe</li>
+          <li>Moddular based</li>
+          <li>Code reusability</li>
+          <li>Components based</li>
+          <li>can build Siingle page Applications</li>
+          <li>can build Progressive web Applications</li>
+          <li>JSX(javascript syntax extension)</li>
+          <li>one way data binding</li>
+          <li>We can Extend the application features</li>
+          <li>Simplicity</li>
+          <li>Condttional Statements</li>
+          <li>Performance of Webapplications is more</li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  );
+}
